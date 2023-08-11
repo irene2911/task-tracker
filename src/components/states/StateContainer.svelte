f825d5fa18ff72d1320c93f477ded9970c6f5f53
irene2911 <irene.trtan1@gmail.com>
2023-08-11 10:49:44 +0200

    add createTask api and createTask logic

--- src/components/states/StateContainer.svelte
+++ src/components/states/StateContainer.svelte
@@ -1,13 +1,21 @@
 <script lang="ts">
-  import type { State } from '$lib/SidebarBoardsStore';
+  import type { SidebarBoard, State } from '$lib/SidebarBoardsStore';
   import Task from './Task.svelte';
   import { flip } from 'svelte/animate';
   import { dndzone } from 'svelte-dnd-action';
+  import axios from 'axios';
+  import { invalidateAll } from '$app/navigation';
+  import { page } from '$app/stores';
 
   export let container: State;
   let inputRef: HTMLInputElement | null = null;
+  let formRef: HTMLFormElement | null = null;
   let showAddInput = false;
+  let taskText: string = '';
 
+  let { boardId } = $page.params;
+
+  // console.log(boardsIds);
   function handleAddInput() {
     showAddInput = !showAddInput;
   }
@@ -15,6 +23,22 @@
   $: {
     if (showAddInput) {
       inputRef?.focus();
+    }
+  }
+
+  async function createTask() {
+    try {
+      await axios.post(`/api/addTasks/${boardId}`, {
+        taskText,
+        containerId: container._id,
+      });
+
+      // TODO: fix me
+      invalidateAll();
+
+      taskText = '';
+    } catch (error) {
+      console.log(error);
     }
   }
 
@@ -37,30 +61,38 @@
 </script>
 
 <section class="flex flex-row w-full justify-evenly gap-5 flex-1">
-  <div class="board">
+  <div class="min-h-[70vh] w-full">
     <div class="h-full flex flex-col gap-4">
       <div
         class="flex flex-col w-full justify-between items-center bg-white rounded-xl"
       >
         <div class="px-5 py-3 flex w-full justify-between">
-          <p class="column-title">{container.name}</p>
+          <p class="flex justify-center items-center">{container.name}</p>
           <div class="flex gap-3">
             <button class="mb-1.5">...</button>
             <button on:click={handleAddInput}>+</button>
           </div>
         </div>
         {#if showAddInput}
-          <div class="pt-5 flex flex-col">
-            <input
-              class="px-3 py-2 rounded-xl bg-gray-100"
-              bind:this={inputRef}
-            />
-            <button class="py-5">Add</button>
-          </div>
+          <form on:submit={createTask} bind:this={formRef}>
+            <div class="pt-5 flex flex-col">
+              <input
+                class="px-3 py-2 rounded-xl bg-gray-100"
+                bind:this={inputRef}
+                bind:value={taskText}
+                on:keypress={(e) => {
+                  if (e.key === 'Enter') {
+                    formRef?.submit();
+                  }
+                }}
+              />
+            </div>
+          </form>
+          <button class="py-5" on:click={createTask}>Add</button>
         {/if}
       </div>
       <div
-        class="column-content"
+        class="h-full overflow-y-scroll flex flex-col gap-3"
         use:dndzone={{
           items,
           flipDurationMs,
@@ -80,43 +112,3 @@
     </div>
   </div>
 </section>
-
-<style>
-  .board {
-    /* height: 70vh; */
-    min-height: 70vh;
-    width: 100%;
-    /* margin-bottom: 40px; */
-  }
-  .column {
-    height: 100%;
-    /* width: 250px; */
-    padding: 0.5em;
-    margin: 1em;
-    float: left;
-    border: 1px solid #333333;
-    /*Notice we make sure this container doesn't scroll so that the title stays on top and the dndzone inside is scrollable*/
-    overflow-y: hidden;
-  }
-  .column-content {
-    height: 100%;
-    /* Notice that the scroll container needs to be the dndzone if you want dragging near the edge to trigger scrolling */
-    overflow-y: scroll;
-  }
-  .column-title {
-    /* margin-bottom: 1em; */
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-  .card {
-    height: 15%;
-    width: 100%;
-    margin: 0.4em 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: #dddddd;
-    /* border: 1px solid #333333; */
-  }
-</style>
--- src/routes/[boardId]/+page.svelte
+++ src/routes/[boardId]/+page.svelte
@@ -2,12 +2,14 @@
   import StateContainer from '../../components/states/StateContainer.svelte';
 
   export let data;
+
+  // console.log(data.boards);
 </script>
 
 {#if !data}
   <p>Loading...</p>
 {:else}
-  <div class="flex w-full flex-col px-10 pt-10 gap-20">
+  <div class="flex w-full flex-col px-10 pt-10 gap-20 mb-10">
     {#if data?.container?.states}
       <h1>{data.container?.name}</h1>
       <div class="flex justify-evenly gap-5 bg-gray-100/80 p-5 rounded-xl">
